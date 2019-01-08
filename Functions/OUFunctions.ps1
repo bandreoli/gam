ab5562aab1ce653bf2e49dbcf1514577de7495a6
Blaine Andreoli <blaine.andreoli70@gmail.com>
2019-01-08 14:03:40 -0500

    added createOU function

--- Functions/OUFunctions.ps1
+++ Functions/OUFunctions.ps1
@@ -4,14 +4,16 @@ function ouOptions {
     while($continue -eq 1) {
         'OU Options'
         '1. Get OU Info'
-        '2. Return'
+        '2. Create OU'
+        '3. Return'
 
         $option = Read-Host -Prompt 'Enter Here'
         ''
 
         switch($option) {
             1 {getOUInfo}
-            2 {$continue = 0}
+            2 {createOU}
+            3 {$continue = 0}
         }
     }
 }
@@ -26,6 +28,32 @@ function getOUInfo {
         gam info org $ou
 
         $continue = Read-Host -Prompt 'Get info from another OU? (y/n)'
+        ''
+    }
+}
+
+function createOU {
+    $continue = 'y'
+
+    while($continue -eq 'y') {
+        'Creating OU'
+
+        $ou = getOU
+
+        $desc = Read-Host -Prompt 'Description'
+        if($desc -ne '') {
+            $descCom = " description '$desc'"
+        }
+        $parent = Read-Host -Prompt 'Parent OU'
+        if($parent -ne '') {
+            $parentCom = " parent '$parent'"
+        }
+
+        $command = "gam create org '$ou' $descCom $parentCom"
+
+        iex $command
+
+        $continue = Read-Host -Prompt 'Create another OU? (y/n)'
         ''
     }
 }
