10257f5f4d4d3371e08e4048f65df6bf75cecc47
bandreoli <blaine.andreoli@yahoo.com>
2019-01-11 09:37:47 -0500

    createSchema functionality added

--- Functions/SchemaFunctions.ps1
+++ Functions/SchemaFunctions.ps1
@@ -34,15 +34,67 @@ function getSchemaInfo {
 
 function createSchema {
     $continue = 'y'
+    $newField = 'y'
 
     while($continue -eq 'y') {
         "Creating Schema"
 
+        $schema = getSchema
 
+        $command = "gam create schema '$schema' "
+
+        ''
+
+        while($newField -eq 'y') {
+            $name = Read-Host -Prompt 'Field Name (Required)'
+
+            'Type:'
+            '1. True/False'
+            '2. Decimal'
+            '3. Email'
+            '4. Integer'
+            '5. Phone'
+            '6. String'
+            $type = ""
+            while($type -eq "") {
+                $type = Read-Host -Prompt 'Enter Here (Required)'
+            }
+
+            switch($type) {
+                1 {$typeCom = "bool"}
+                2 {$typeCom = "double"}
+                3 {$typeCom = "email"}
+                4 {$typeCom = "int64"}
+                5 {$typeCom = "phone"}
+                6 {$typeCom = "string"}
+            }
+
+            $multivalueCom = ""
+
+            $multivalue = Read-Host -Prompt 'Mulitvalued? (y/n)'
+            if($multivalue -eq 'y') {
+                $multivalueCom = 'multivalued'
+            }
+
+            $restrictedCom = ""
+
+            $restricted = Read-Host -Prompt 'Restricted to Admin? (y/n)'
+            if($restricted -eq 'y') {
+                $restrictedCom = 'restricted'
+            }
+
+            $command = $command + "field '$name' type $typeCom '$multivalueCom' '$restrictedCom' endfield "
+
+            $newField = Read-Host -Prompt 'Add another field? (y/n)'
+            ''
+        }
+
+        iex $command
 
         $continue = Read-Host -Prompt 'Create another Schema? (y/n)'
         ''
     }
+}
 
 function getSchema {
     $schema = ""
